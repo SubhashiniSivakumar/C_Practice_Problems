38cab4a93dcc1d4f17608e806d06c47ca3e54025
Subhashini S <subhashinisiva16@gmail.com>
2025-09-17 21:38:05 +0530

    Create Quadrant.c

---
+++ Quadrant.c
@@ -0,0 +1,19 @@
+// Write a program to determine the quadrant of a point (x, y) in the Cartesian plane.
+
+#include<stdio.h>
+int main(){
+    int x,y;
+    scanf("%d %d",&x,&y);
+    if(x>0 && y>0){
+        printf("1");
+    }
+    else if(x<0 && y<0){
+        printf("3");
+    }
+    else if(x>0 && y<0){
+        printf("4");
+    }
+    else{
+        printf("2");
+    }
+}
